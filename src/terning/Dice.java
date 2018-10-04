3de9ebe17fcc639bea982781bf0227cc0d0a707d
jdkramhoft <s175445@student.dtu.dk>
2018-10-04 18:22:07 +0200

    Cleaned up Dice.java

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -3,14 +3,17 @@ package terning;
 import java.util.Random;
 
 public class Dice {
-    // roll the die and return the value (1-6)
+
+    // Roll the dice and return a value from 1 to 6
     public int roll() {
         Random random = new Random();
+
         // Rolls random number between 0(inclusive) and 6(exclusive)
         int Result = random.nextInt(6);
         return Result+1;
     }
-    // roll the die n times and print the values
+
+    // Roll the dice n times and print the values
     public void rollMultiple(int n) {
         for (int i=1; i<=n; i++) {
             System.out.print(roll() + " ");
